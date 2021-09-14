0bd80029759e4e3ffeab5b005c1ebeca4262634e
AdaptCharm <jamie@jamie.no>
2021-09-14 22:01:27 +0200

    Remove loading when page has rendered

--- css-frameworks-ca/pages/_app.tsx
+++ css-frameworks-ca/pages/_app.tsx
@@ -1,10 +1,14 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
 const Noop: FC = ({ children }) => <>{children}</>
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const Layout = (Component as any).Layout || Noop
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
 
   return (
     <>
