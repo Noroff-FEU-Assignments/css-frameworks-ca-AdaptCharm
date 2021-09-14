ea3e232fd3eedb73bad8ce6663da90ec2cf46cf2
AdaptCharm <jamie@jamie.no>
2021-09-14 21:58:24 +0200

    Minor changes to the app component

--- css-frameworks-ca/pages/_app.tsx
+++ css-frameworks-ca/pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { FC } from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const Noop: FC = ({ children }) => <>{children}</>
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const Layout = (Component as any).Layout || Noop
+
+  return (
+    <>
+      <Layout pageProps={pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
+
 export default MyApp
